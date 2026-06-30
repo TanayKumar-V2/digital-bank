786d0322a426a77ec0ac77e7710e48d7173ecde8
TanayKumar-V2 <tanayk726@gmail.com>
2026-06-30 21:04:38 +0530

    added global exception

---
+++ auth-service/src/main/java/com/neobank/auth_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.neobank.auth_service.exception;
+
+import com.neobank.auth_service.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyExistsException(
+            EmailAlreadyExistsException ex
+    ) {
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                ex.getMessage()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(error);
+    }
+}
