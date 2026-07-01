06c8fe347d205b37211fea89c03ed8cd56dae795
TanayKumar-V2 <tanayk726@gmail.com>
2026-07-02 00:40:20 +0530

    Added refresh token entity and repo

---
+++ auth-service/src/main/java/com/neobank/auth_service/entity/RefreshToken.java
@@ -0,0 +1,51 @@
+package com.neobank.auth_service.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "refresh_tokens")
+public class RefreshToken {
+
+    @Id
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String tokenHash;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime lastUsedAt;
+
+    @Column(nullable = false)
+    private Boolean revoked;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @PrePersist
+    public void prePersist() {
+
+        if (id == null) {
+            id = UUID.randomUUID();
+        }
+
+        createdAt = LocalDateTime.now();
+
+        revoked = false;
+    }
+}
